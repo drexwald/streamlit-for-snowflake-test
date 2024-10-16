cd87ac1a0c93ac79644585e2b769f558edd4a977
Okwudili Ikeche <Philip.Ikeche@kiewit.com>
2024-10-16 12:52:17 -0400

    update

--- 4-deploy-to-snowflake/02-data-viewer-streamlit-app/deploy.sql
+++ 4-deploy-to-snowflake/02-data-viewer-streamlit-app/deploy.sql
@@ -1,19 +1,19 @@
 -- to be deployed as a Streamlit App with: snowsql -c demo_conn -f deploy.sql
 !set variable_substitution=true
-!define CRT_DIR=file://C:\Projects\streamlit-for-snowflake-test\data-viewer-snowflake
+!define CRT_DIR='file:///Users/pikeche/Documents/snowflake/udemy/streamlit-for-snowflake-test/4-deploy-to-snowflake/02-data-viewer-streamlit-app'
 
-use schema tests.public;
+use schema sandbox.pikeche;
 
 create stage if not exists streamlit_stage;
 
-put &CRT_DIR\app.py @streamlit_stage overwrite=true auto_compress=false;
-put &CRT_DIR\modules\*.py @streamlit_stage/modules overwrite=true auto_compress=false;
-put &CRT_DIR\data\*.csv @streamlit_stage/data overwrite=true auto_compress=false;
+put &CRT_DIR/app.py @streamlit_stage overwrite=true auto_compress=false;
+put '&CRT_DIR/modules/*.py' @streamlit_stage/modules overwrite=true auto_compress=false;
+put '&CRT_DIR/data/*.csv' @streamlit_stage/data overwrite=true auto_compress=false;
 -- put &CRT_DIR\animated\templates\*.html @streamlit_stage/animated/templates overwrite=true auto_compress=false;
-put &CRT_DIR\environment.yml @streamlit_stage overwrite=true auto_compress=false;
+put '&CRT_DIR/environment.yml' @streamlit_stage overwrite=true auto_compress=false;
 
 create or replace streamlit data_viewer_snowflake
-    root_location = '@tests.public.streamlit_stage'
+    root_location = '@sandbox.pikeche.streamlit_stage'
     main_file = '/app.py'
-    query_warehouse = 'COMPUTE_WH';
+    query_warehouse = 'SANDBOX_WH';
 show streamlits;
